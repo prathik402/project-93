fcea03fc818d02d788c47b855f54873ffaa32d31
prathik402 <73998548+prathik402@users.noreply.github.com>
2022-10-05 16:43:46 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrZl2fBrrqli4p-s2_eCCEghDZhJCiNYM",
+  authDomain: "cooking-app-56cdb.firebaseapp.com",
+  databaseURL: "https://cooking-app-56cdb-default-rtdb.firebaseio.com",
+  projectId: "cooking-app-56cdb",
+  storageBucket: "cooking-app-56cdb.appspot.com",
+  messagingSenderId: "354241604361",
+  appId: "1:354241604361:web:2611ab88cd4668b73601d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
